e77679919a769fe119c60f97b0c4f05877f87a10
sherllyneo <sherlly.neomail@gmail.com>
2023-01-02 02:26:14 +0000

    constructing dataframe

--- companies_house_api.py
+++ companies_house_api.py
@@ -1,3 +1,4 @@
+import re
 import requests
 from requests.auth import HTTPBasicAuth
 import pandas as pd
@@ -10,10 +11,15 @@ API_KEY = os.getenv("KEY")
 
 
 class CH:
+
     def __init__(self):
         self.auth = HTTPBasicAuth(username=API_KEY,password="")
         self.search_url = "https://api.company-information.service.gov.uk/advanced-search/companies"
         self.company_info_url = "https://api.company-information.service.gov.uk/company/"
+        self.tb = pd.read_html('https://developer-specs.company-information.service.gov.uk/companies-house-public-data-api/resources/companyprofile?v=latest')[0]
+        self.columns = list(map(lambda x: re.sub("\.","_",x),self.tb[self.tb.Type != 'object'].Name.tolist()))
+        
+        
 
     def search(self,name_inc="",name_ex="",company_status=None,company_sub_typ=None,company_typ=None,diss_from=None,diss_to=None,inc_from=None,inc_to=None,location=None,sic_codes=[],result_size="50",index_start=None) -> list:
         """ wrapped for company house api advanced search """
@@ -33,13 +39,16 @@ class CH:
         'start_index':index_start,
         }
         response = requests.get(url=self.search_url,params=params,auth=self.auth)
+        print(response)
         company_numbers =  list(map(lambda x: x['company_number'],response.json()['items']))
         return company_numbers
 
     def get_company_data(self,company_number: str) -> pd.DataFrame:
         """ helper method to get dataframe for a company """
         response = requests.get(url=self.company_info_url+company_number,auth=self.auth)
-        return response.json()
+        ch_data = defaultdict(str,response.json())
+        print(self.columns)
+        return ch_data
 
     def make_dataframe_from_numbers(self,company_numbers_list: list) -> pd.DataFrame:
         """ gets more detailed information from each company from their companies house number and returns it as a dataframe  """
